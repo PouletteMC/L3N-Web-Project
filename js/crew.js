fc95031c7b1131945d4727047c6c684ed30d859f
Poulette <eliotb1357@gmail.com>
2023-01-04 08:56:57 +0100

    Updated footer date and crew page

--- js/crew.js
+++ js/crew.js
@@ -25,25 +25,47 @@ function handleData(crewMembers){
 }
 
 function appendData(data){
-    const newRow = document.createElement('tr')
-    const image = document.createElement('td')
-    const name = document.createElement('td')
-    const agency = document.createElement('td')
-    const wikipedia = document.createElement('td')
+    // const newRow = document.createElement('tr')
+    // const image = document.createElement('td')
+    // const name = document.createElement('td')
+    // const agency = document.createElement('td')
+    // const wikipedia = document.createElement('td')
 
-    name.classList.add('name')
-    agency.classList.add('agency')
+    // name.classList.add('name')
+    // agency.classList.add('agency')
 
-    image.innerHTML = `<img src="${data.image}" alt="${data.name}" class="image" >`
-    name.innerText = data.name
-    agency.innerText = data.agency
-    wikipedia.innerHTML = `<a href="${data.wikipedia}" target="_blank" class='button' >Read more</a>`
+    // image.innerHTML = `<img src="${data.image}" alt="${data.name}" class="image" >`
+    // name.innerText = data.name
+    // agency.innerText = data.agency
+    // wikipedia.innerHTML = `<a href="${data.wikipedia}" target="_blank" class='button' >Read more</a>`
 
-    newRow.appendChild(image)
-    newRow.appendChild(name)
-    newRow.appendChild(agency)
-    newRow.appendChild(wikipedia)
+    // newRow.appendChild(image)
+    // newRow.appendChild(name)
+    // newRow.appendChild(agency)
+    // newRow.appendChild(wikipedia)
 
-    document.querySelector('tbody').appendChild(newRow)
-    document.getElementById('count').innerText = `Total: ${data.count}`
+    // document.querySelector('tbody').appendChild(newRow)
+    // document.getElementById('count').innerText = `Total: ${data.count}`
+
+    const card = document.createElement('div')
+    const cardInner = document.createElement('div')
+    const cardFront = document.createElement('div')
+    const cardBack = document.createElement('div')
+
+    cardInner.classList.add('flip-card-inner')
+    cardFront.classList.add('flip-card-front')
+    cardBack.classList.add('flip-card-back')
+
+    cardFront.innerHTML = `<img src="${data.image}" alt="${data.name}" class="image" >`
+    cardBack.innerHTML = `<h2>${data.name}</h2> <p>${data.agency}</p> <a href="${data.wikipedia}" target="_blank" class='button' >Read more</a>`
+
+    card.classList.add('flip-card')
+    card.appendChild(cardInner)
+    cardInner.appendChild(cardFront)
+    cardInner.appendChild(cardBack)
+
+    document.getElementById('crew').appendChild(card)
+
+
+
 }
